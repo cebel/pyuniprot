3bd6aa8828b4dbbc2d0b8c126ba4d1877f0b93cf
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-08-11 13:54:38 +0200

    Add update function to CLI
    Closes #8

--- src/pyuniprot/cli.py
+++ src/pyuniprot/cli.py
@@ -1,11 +1,15 @@
 import click
+from .manager.database import update
 from .webserver.web import get_app
-
 
 @click.group()
 def main():
     pass
 
+@main.command()
+def update():
+    """Update PyUniProt data"""
+    pyuniprot.update()
 
 @main.command()
 def web():
